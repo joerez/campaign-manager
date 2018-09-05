cc86d73ea9e6e1da9016ef5b85e99f11f6a9ec94
Joerez <rezenbiz@gmail.com>
2018-09-05 10:40:48 -0700

    emails now get sent to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {emails: []}
+
+  }
+
+  componentDidMount() {
+    axios.get('/api/emails').then((res) => {
+      this.setState({emails : res.data})
+    })
+  }
+
+  renderEmails() {
+    return this.state.emails.reverse().map(email => {
+      return (
+        <div className="card darken-1" key={email._id}>
+          <div className="card-content">
+            <span className="card-title">{email.title}</span>
+            <p>
+              {email.subject}
+              {email.body}
+            </p>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="dashboard">
+
+        {this.renderEmails()}
+
         <Link to="/email/new"><button className="action-btn">New Email</button></Link>
+
+
       </div>
     )
   }
